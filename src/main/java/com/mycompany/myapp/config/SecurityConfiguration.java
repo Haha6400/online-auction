da2ce25be9424c3c27d2862ba92a1206f268f782
Tan Duc <ductan10092003@gmail.com>
2024-04-08 23:23:48 +0700

    change email port

--- src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
@@ -51,6 +51,7 @@ public class SecurityConfiguration {
                     .requestMatchers(mvc.pattern("/api/account/reset-password/init")).permitAll()
                     .requestMatchers(mvc.pattern("/api/account/reset-password/finish")).permitAll()
                     .requestMatchers(mvc.pattern("/api/admin/**")).hasAuthority(AuthoritiesConstants.ADMIN)
+                    .requestMatchers(mvc.pattern("/api/auction-rooms")).hasAuthority(AuthoritiesConstants.ADMIN)
                     .requestMatchers(mvc.pattern("/api/**")).authenticated()
                     .requestMatchers(mvc.pattern("/ws/**")).permitAll()
                     .requestMatchers(mvc.pattern("/v3/api-docs/**")).hasAuthority(AuthoritiesConstants.ADMIN)
